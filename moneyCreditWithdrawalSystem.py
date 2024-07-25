1ac0e33f45aa290c138fa881a390dd06d7ca15f4
KiranUndru <kirankumarundru18@gmial.com>
2024-07-25 13:33:31 +0530

    First_commit

---
+++ moneyCreditWithdrawalSystem.py
@@ -0,0 +1,12 @@
+class moneyCreditWithdrawlSystem:
+    # pass
+    if __name__ == '__main__':
+        print('Hi,Welcome to Money Credit Witdral System')
+        print('I am assesting for credit and withdrawl money from your bank account')
+        print('''Please select your options in below
+              1.Enter 1 for Registration
+              2.Enter 2 for Login
+              ''')
+        registration_login_cmd = int(int())
+        print(f"{registration_login_cmd}")
+        
